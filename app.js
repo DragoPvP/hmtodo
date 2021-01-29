d6c0c35d45dbe938150bd48081e815c2c5666c86
Hesham Khanheroku <heshamkhan20@gmail.com>
2021-01-29 19:23:35 +0400

    khan

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-
+ 
 app.listen(port, function() {
-  console.log("Server started on port Successfully!");
+  console.log("Server started succesfully");
 });
